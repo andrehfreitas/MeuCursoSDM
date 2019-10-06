119b7d61456df3f1110e66e4148e1c7f04eecea2
andrehfreitas <andrehfreitas@gmail.com>
2019-10-06 13:37:36 -0300

    Parte 06 - Passo 05

--- app/src/main/java/br/edu/ifsp/scl/meucursosdm/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/meucursosdm/view/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerForContextMenu(disciplinasLv)
 
         // lista de disciplinas do Adapter
         listaDisciplinas = mutableListOf()
