cb3708911584cce4b79d3a414c7f8fcf458d26ca
andrehfreitas <andrehfreitas@gmail.com>
2019-10-06 12:03:43 -0300

    Parte 04 - Passo 02

---
+++ app/src/main/java/br/edu/ifsp/scl/meucursosdm/controller/CursoController.kt
@@ -0,0 +1,31 @@
+package br.edu.ifsp.scl.meucursosdm.controller
+
+import br.edu.ifsp.scl.meucursosdm.MainActivity
+import br.edu.ifsp.scl.meucursosdm.model.Disciplina
+import br.edu.ifsp.scl.meucursosdm.model.DisciplinaSqlite
+
+class CursoController(val mainActivity: MainActivity) {
+    val disciplinaDao: DisciplinaDao
+    init {
+        disciplinaDao = DisciplinaSqlite(mainActivity)
+    }
+    fun insereDisciplina(disciplina: Disciplina){
+        disciplinaDao.createDisciplina(disciplina)
+        buscaTodas()
+    }
+    fun buscaDisciplina(codigo: String){
+        val disciplina: Disciplina = disciplinaDao.readDisciplina(codigo)
+        mainActivity.atualizaLista(mutableListOf(disciplina))
+    }
+    fun buscaTodas() {
+        mainActivity.atualizaLista(disciplinaDao.readDisciplinas())
+    }
+    fun atualiza(disciplina: Disciplina) {
+        disciplinaDao.updateDisciplina(disciplina)
+        buscaTodas()
+    }
+    fun remove(codigo: String) {
+        disciplinaDao.deleteDisciplina(codigo)
+        buscaTodas()
+    }
+}
